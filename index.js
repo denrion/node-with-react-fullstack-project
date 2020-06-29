f09d7ccb7150c3f663b8c4983458f889b7cfd098
Nemanja <kuzmaneca@gmail.com>
2020-06-29 14:33:24 +0200

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running in ${process.env.NODE_ENV} env, on port ${PORT}`);
+});
