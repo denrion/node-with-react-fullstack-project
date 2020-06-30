7b52687f6485f623123a3b3d084f07e5ffeaa4a9
Nemanja <kuzmaneca@gmail.com>
2020-06-30 18:31:28 +0200

    implemented the passport auth flow

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
+const MONGO_URI = process.env.MONGODB_ATLAS_URI || keys.mongoURI;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error(err));
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milis
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
